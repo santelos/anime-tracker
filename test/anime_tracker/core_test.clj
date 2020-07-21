e493d10f12a1040ec1fe7e40c3fefdac6712a87f
PStroganov <petr.stroganov@gmail.com>
2020-07-21 12:09:08 +0300

    Инициализация приложения с leiningein

---
+++ test/anime_tracker/core_test.clj
@@ -0,0 +1,7 @@
+(ns anime-tracker.core-test
+  (:require [clojure.test :refer :all]
+            [anime-tracker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
