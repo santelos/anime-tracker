b286b5c33c00d12ae020f6ed0c9b9bda17adcb77
PStroganov <petr.stroganov@gmail.com>
2020-07-21 14:39:27 +0300

    Добавление зависимости на JDBC

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.8.1"]
                  [ring/ring-jetty-adapter "1.8.1"]
-                 [metosin/reitit-ring "0.5.5"]]
+                 [metosin/reitit-ring "0.5.5"]
+                 [org.clojure/java.jdbc "0.7.11"]]
   :main ^:skip-aot anime-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
