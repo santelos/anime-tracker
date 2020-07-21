da53dc4704c094f34a6864d1dec584dd4ad40727
PStroganov <petr.stroganov@gmail.com>
2020-07-21 12:32:30 +0300

    Hello World HTTP handler

--- src/anime_tracker/core.clj
+++ src/anime_tracker/core.clj
@@ -1,7 +1,15 @@
 (ns anime-tracker.core
-  (:gen-class))
+  (:gen-class)
+    (:use ring.adapter.jetty))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello World"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Server is starting up")
+  (run-jetty handler {:port 8080}))
+
