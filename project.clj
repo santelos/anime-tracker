543e1be0cc4211c314138eb90a06ab1adc745a09
Danone <crimsonandred@mail.ru>
2020-07-21 13:12:21 +0300

    Нерабочий вариант reitit

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.8.1"]
-                 [ring/ring-jetty-adapter "1.8.1"]]
+                 [ring/ring-jetty-adapter "1.8.1"]
+                 [metosin/reitit "0.5.5"]]
   :main ^:skip-aot anime-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/anime_tracker/core.clj
+++ src/anime_tracker/core.clj
@@ -1,15 +1,25 @@
+
 (ns anime-tracker.core
   (:gen-class)
-    (:use ring.adapter.jetty))
+    (:use ring.adapter.jetty
+          ring.util.response
+          reitit.core))
+
 
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello World"})
+  (content-type (file-response "index.html" {:root "resources"}) "text/html"))
+(defn ping [request]
+  (content-type (file-response "index.html" {:root "resources"}) "text/html"))
 
+(defn order [request]
+  (content-type (file-response "huindex.html" {:root "resources"}) "text/html"))
+
+(def rr
+  (reitit.core/router
+    [["/api/ping" ::ping]
+     ["/api/orders" ::order]]))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Server is starting up")
-  (run-jetty handler {:port 8080}))
-
+  (run-jetty rr {:port 8080}))
