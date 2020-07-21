c88d6aa5435501880d7ead9829a9cdef21008ce7
PStroganov <petr.stroganov@gmail.com>
2020-07-21 13:51:02 +0300

    Рабочий варик роутинга

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.8.1"]
                  [ring/ring-jetty-adapter "1.8.1"]
-                 [metosin/reitit "0.5.5"]]
+                 [metosin/reitit-ring "0.5.5"]]
   :main ^:skip-aot anime-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/anime_tracker/core.clj
+++ src/anime_tracker/core.clj
@@ -3,23 +3,25 @@
   (:gen-class)
     (:use ring.adapter.jetty
           ring.util.response
-          reitit.core))
+          reitit.ring))
 
 
 (defn handler [request]
   (content-type (file-response "index.html" {:root "resources"}) "text/html"))
-(defn ping [request]
-  (content-type (file-response "index.html" {:root "resources"}) "text/html"))
 
-(defn order [request]
-  (content-type (file-response "huindex.html" {:root "resources"}) "text/html"))
+;(defn ping [request]
+;  (content-type (file-response "index.html" {:root "resources"}) "text/html"))
+;
+;(defn order [request]
+;  (content-type (file-response "huindex.html" {:root "resources"}) "text/html"))
 
-(def rr
-  (reitit.core/router
-    [["/api/ping" ::ping]
-     ["/api/orders" ::order]]))
+(def app
+  (reitit.ring/ring-handler
+   (reitit.ring/router
+    [["/all" handler]])))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Server is starting up")
-  (run-jetty rr {:port 8080}))
+  (run-jetty #'app {:port 8080}))
