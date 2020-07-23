f316d334d6fd109966b63ec7dfbc338ea92548b8
Danone <crimsonandred@mail.ru>
2020-07-23 21:48:05 +0300

    Исправление формы для обработки текста

--- src/anime_tracker/mapping.clj
+++ src/anime_tracker/mapping.clj
@@ -14,4 +14,4 @@
   (map instruct-users-to-title (seq (group-by mapping-by-title titles))))
 
 (defn extract-title-from-form [title]
-  (hash-map :name (title "name") :link (title "link") :name (title "name") :original_name (title "original_name") :watched_series (read-string (title "watched_series")) :total_series (read-string (title "total_series")) :status (read-string (title "status"))))
+  (hash-map :name (name (title "name")) :link (name (title "link")) :name (name (title "name")) :original_name (name (title "original_name")) :watched_series (read-string (title "watched_series")) :total_series (read-string (title "total_series")) :status (read-string (title "status"))))
