e27c79bdf78d799bc2eb1a707b97dfa295f90ff3
Danone <crimsonandred@mail.ru>
2020-07-23 22:00:09 +0300

    Небольшие правки по отоброжению

--- src/anime_tracker/mapping.clj
+++ src/anime_tracker/mapping.clj
@@ -4,14 +4,14 @@
   (hash-map :id (title :id) :original_name  (title :original_name) :name (title :name) :status (title :status) :link (title :link)  :watched_series (title :watched_series) :total_series (title :total_series)))
 
 (defn extract-user [user]
-  (hash-map :id (user :id) :name (user :name_2)))
+  (hash-map :id (user :id_2) :name (user :name_2)))
 
 (defn instruct-users-to-title [titles-with-users]
   (let [[title users] titles-with-users]
-    (assoc title :users (into [] (map extract-user users)))))
+    (assoc title :users (sort-by :id (into [] (map extract-user users))))))
 
 (defn map-titles [titles]
-  (map instruct-users-to-title (seq (group-by mapping-by-title titles))))
+  (sort-by :id (map instruct-users-to-title (seq (group-by mapping-by-title titles)))))
 
 (defn extract-title-from-form [title]
   (hash-map :name (name (title "name")) :link (name (title "link")) :name (name (title "name")) :original_name (name (title "original_name")) :watched_series (read-string (title "watched_series")) :total_series (read-string (title "total_series")) :status (read-string (title "status"))))
