98829502ec1f9a4ef03e710af646ede617117bdb
Danone <crimsonandred@mail.ru>
2020-07-27 19:09:44 +0300

    Настройка БД для heroku

--- src/anime_tracker/config.clj
+++ src/anime_tracker/config.clj
@@ -6,7 +6,7 @@
   (read-string (or (env :port) "8080")))
 
 (defn database-url []
-  (or (env :db-url) "jdbc:postgresql://localhost:5432/postgres"))
+  (or (env :database-url) "jdbc:postgresql://localhost:5432/postgres"))
 
 (defn database-password []
   (or (env :db-password) "postgres"))
