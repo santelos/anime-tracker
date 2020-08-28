016729eb087feba0be0d98d35c844c978c7614c6
Petr Stroganov <petr.stroganov@gmail.com>
2020-08-29 00:28:26 +0300

    Update project.clj
    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject anime-tracker "0.1.0-SNAPSHOT"
+(defproject anime-tracker "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.1"]
